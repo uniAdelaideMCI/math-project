345fd39b00fa66da000267ecdbaa7011d9898dac
Yun Zhang <jslgdxzy@gmail.com>
2015-04-22 10:37:46 +0930

    save state across rotation

--- self-learn-math/src/com/example/geoquiz/QuizActivity.java
+++ self-learn-math/src/com/example/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,12 +12,15 @@ import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
 
+	private static final String TAG = "QuizActivity";
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
 	private TextView mQuestionTextView;
 
 	private int mCurrentIndex = 0;
+	
+	private static final String KEY_INDEX = "index";
 
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
 			new TrueFalse(R.string.question_oceans, true),
@@ -28,9 +32,15 @@ public class QuizActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 
 		mQuestionTextView = (TextView) findViewById(R.id.quetion_text_view);
+		
+		if (savedInstanceState != null){
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+		}
 		updateQuestion();
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
@@ -94,5 +104,45 @@ public class QuizActivity extends ActionBarActivity {
 			messageResId = R.string.incorrect_toast;
 		}
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os.Bundle)
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "onSaveInstanceState");
+		outState.putInt(KEY_INDEX, mCurrentIndex);
 	}
 }
