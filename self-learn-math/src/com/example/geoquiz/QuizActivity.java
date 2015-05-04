63535d8b38b4ba108951148ede5f9a10f447438c
YunZhang2014 <jslgdxzy@gmail.com>
2015-05-04 11:44:15 +0930

    update self-learn file
    update self-learn file

--- self-learn-math/src/com/example/geoquiz/QuizActivity.java
+++ self-learn-math/src/com/example/geoquiz/QuizActivity.java
@@ -1,7 +1,6 @@
 package com.example.geoquiz;
 
-import android.app.ActionBar;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -18,11 +17,23 @@ public class QuizActivity extends ActionBarActivity {
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
+	private Button mCheatButton;
 	private TextView mQuestionTextView;
 
 	private int mCurrentIndex = 0;
-	
+
+	private boolean mIsCheater = false;
+
 	private static final String KEY_INDEX = "index";
+
+	@Override
+	protected void onActivityResult(int arg0, int arg1, Intent data) {
+		if (data == null) {
+			return;
+		}
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN,
+				false);
+	}
 
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
 			new TrueFalse(R.string.question_oceans, true),
@@ -38,16 +49,31 @@ public class QuizActivity extends ActionBarActivity {
 		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 
-//		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-////			ActionBar actionBar = getActionBar();
-////			actionBar.setSubtitle("Bodies of Water");	
-//		}
-		
+		// if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+		// // ActionBar actionBar = getActionBar();
+		// // actionBar.setSubtitle("Bodies of Water");
+		// }
+
 		mQuestionTextView = (TextView) findViewById(R.id.quetion_text_view);
-		
-		if (savedInstanceState != null){
-			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+
+		if (savedInstanceState != null) {
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
 		}
+
+		mCheatButton = (Button) findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex]
+						.isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+
+				startActivityForResult(i, 0);
+			}
+		});
+
 		updateQuestion();
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
@@ -105,11 +131,16 @@ public class QuizActivity extends ActionBarActivity {
 	private void checkAnswer(boolean userPressedTrue) {
 		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 		int messageResId = 0;
-		if (userPressedTrue == answerIsTrue) {
-			messageResId = R.string.correct_toast;
+		if (mIsCheater) {
+			messageResId = R.string.judgment_toast;
 		} else {
-			messageResId = R.string.incorrect_toast;
+			if (userPressedTrue == answerIsTrue) {
+				messageResId = R.string.correct_toast;
+			} else {
+				messageResId = R.string.incorrect_toast;
+			}
 		}
+
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 	}
 
@@ -143,8 +174,12 @@ public class QuizActivity extends ActionBarActivity {
 		Log.d(TAG, "onDestroy() called");
 	}
 
-	/* (non-Javadoc)
-	 * @see android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os.Bundle)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os
+	 * .Bundle)
 	 */
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
