a04c89ac8165eae53b27d84b600e91c44e16296e
YunZhang2014 <jslgdxzy@gmail.com>
2015-05-12 11:33:36 +0930

    change apples in count to balloons

--- kid-numeracy/src/au/edu/adelaide/mci/kidnumeracy/ImageAdapter.java
+++ kid-numeracy/src/au/edu/adelaide/mci/kidnumeracy/ImageAdapter.java
@@ -1,8 +1,13 @@
 package au.edu.adelaide.mci.kidnumeracy;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,11 +19,28 @@ public class ImageAdapter extends BaseAdapter {
 	
 	private ImageView[] imageViews; 
 	
+	private CountLearning countLearning;
+	
+	//MediaPlayer mediaPlayer;
+	
+	private final static int FRAME_DELAY = 150;
+	
+	private Map<ImageView,Integer> counterNums = new HashMap<ImageView,Integer>(); 
+	
 	private int maxValue;
 	
 	public ImageAdapter(Context c) {
         mContext = c;
         CountLearnActivity countLearnActivity = (CountLearnActivity)c;
+        countLearning = countLearnActivity.getCountLearning();
+        //mediaPlayer = MediaPlayer.create(countLearnActivity, R.raw.bubble_explode);
+//        try {
+//			mediaPlayer.prepare();
+//		} catch (IllegalStateException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
         maxValue = countLearnActivity.getCountLearning().getMaxValue();
         imageViews = new ImageView[maxValue];
     }	
@@ -37,6 +59,76 @@ public class ImageAdapter extends BaseAdapter {
 	public long getItemId(int position) {
 		return 0;
 	}
+	
+	private Drawable getDrawableByValue(int value) {
+		int resId = R.drawable.n0;
+		switch (value) {
+		case 1:
+			resId = R.drawable.n1;
+			break;
+		case 2:
+			resId = R.drawable.n2;
+			break;
+		case 3:
+			resId = R.drawable.n3;
+			break;
+		case 4:
+			resId = R.drawable.n4;
+			break;
+		case 5:
+			resId = R.drawable.n5;
+			break;	
+		case 6:
+			resId = R.drawable.n6;
+			break;
+		case 7:
+			resId = R.drawable.n7;
+			break;
+		case 8:
+			resId = R.drawable.n8;
+			break;
+		case 9:
+			resId = R.drawable.n9;
+			break;
+		case 10:
+			resId = R.drawable.n10;
+			break;
+		case 11:
+			resId = R.drawable.n11;
+			break;
+		case 12:
+			resId = R.drawable.n12;
+			break;
+		case 13:
+			resId = R.drawable.n13;
+			break;
+		case 14:
+			resId = R.drawable.n14;
+			break;
+		case 15:
+			resId = R.drawable.n15;
+			break;
+		case 16:
+			resId = R.drawable.n16;
+			break;
+		case 17:
+			resId = R.drawable.n17;
+			break;
+		case 18:
+			resId = R.drawable.n18;
+			break;	
+		case 19:
+			resId = R.drawable.n19;
+			break;
+		case 20:
+			resId = R.drawable.n20;
+			break;				
+		default:
+			break;
+		}
+		
+		return mContext.getResources().getDrawable(resId);
+	}	
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -44,17 +136,32 @@ public class ImageAdapter extends BaseAdapter {
     	imageViews[position].setImageResource(R.drawable.balloon1);
     	AnimationDrawable animationDrawable = new AnimationDrawable();
     	Drawable balloon1 = mContext.getResources().getDrawable(R.drawable.balloon1);
-    	animationDrawable.addFrame(balloon1, 600);
-    	Drawable balloon2 = mContext.getResources().getDrawable(R.drawable.balloon2);
-    	animationDrawable.addFrame(balloon2, 600);
+    	animationDrawable.addFrame(balloon1, FRAME_DELAY);
+//    	Drawable balloon2 = mContext.getResources().getDrawable(R.drawable.balloon2);
+//    	animationDrawable.addFrame(balloon2, FRAME_DELAY);
     	Drawable balloon3 = mContext.getResources().getDrawable(R.drawable.balloon3);
-    	animationDrawable.addFrame(balloon3, 600);
+    	animationDrawable.addFrame(balloon3, FRAME_DELAY);
+    	animationDrawable.setOneShot(true);
     	imageViews[position].setImageDrawable(animationDrawable);
-    	animationDrawable.start();
+    	animationDrawable.stop();
     	imageViews[position].setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-				
+				if (v != null && v instanceof ImageView){
+					ImageView imageView = (ImageView)v;
+					Drawable drawable = imageView.getDrawable();
+					if (drawable instanceof AnimationDrawable){
+						AnimationDrawable  animationDrawable = (AnimationDrawable)drawable;
+						if (counterNums.get(imageView) == null){
+							int newValue = countLearning.nextValue();
+							counterNums.put(imageView,newValue);
+							animationDrawable.addFrame(getDrawableByValue(newValue), FRAME_DELAY);
+//							mediaPlayer.stop();
+//							mediaPlayer.start();
+							animationDrawable.start();							
+						}
+					}
+				}
 			}
 		});
     	return imageViews[position];
